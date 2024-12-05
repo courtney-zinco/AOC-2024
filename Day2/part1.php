1c6c4d4e2726c49aa80e272b1b98330978eeb650
courtney-zinco <courtney.henderson@impactinternational.com>
2024-12-05 09:23:17 +0000

    Add commentary to day 2
    Add commentary to day 2, part 1 and part 2

--- Day2/part1.php
+++ Day2/part1.php
@@ -1,57 +1,68 @@
 <?php
 
+// Open the input file in read mode
 $input_data = fopen("./input.txt", "r");
 
+// Initialize an array to keep track of safety messages
 $safeTracker = [];
+// Initialize a counter to keep track of the number of safe sequences
 $safeSum = 0;
 
+// Read the file line by line
 while (($line = fgets($input_data)) !== false) {        
+    // Split the line into an array of levels
     $levels = explode(" ", $line);
         
+    // Initialize the first level as the track
     $track = $levels[0];
+    // Flag to indicate if the sequence is decreasing
     $decreasing = false;
+    // Flag to indicate if the sequence is safe
     $isSafe = true;
 
+    // Check if the first level is greater than the second level
     if($levels[0] > $levels[1]) {
+        // If so, set the decreasing flag to true
         $decreasing = true;
     }
     
-
+    // Iterate through each level in the sequence
     foreach($levels as $key => $level) {
-
+        // Skip the first level as it is already set as the track
         if($key == 0) {
             continue;
         }
 
-        // Check we are still decreasing if we started decreasing
+        // Check if the sequence is still decreasing if it started decreasing
         if($decreasing && $level > $track) {
+            // If not, mark the sequence as not safe and add a message to the tracker
             $safeTracker[] = 'Not decreasing or increasing properly 1';
             $isSafe = false;
             break;            
-        }elseif(!$decreasing && $level < $track) {
+        } elseif(!$decreasing && $level < $track) {
+            // If the sequence is supposed to be increasing but a level is less than the previous level
+            // mark the sequence as not safe and add a message to the tracker
             $isSafe = false;
             $safeTracker[] = 'Not decreasing or increasing properly 2';
             break;
         }
 
-        // Check the value bwtween the numbers change is not greater than 3 and is more than 1
+        // Check if the difference between consecutive levels is greater than 3 or less than 1
         if(abs($track - $level) > 3 || abs($track - $level) < 1) {            
+            // If so, mark the sequence as not safe and add a message to the tracker
             $isSafe = false;
             $safeTracker[] = 'Difference between levels is greater than 3 or less than 1, it is: '.abs($track - $level);
             break;
         }
+        // Update the track to the current level
         $track = $level;        
     }    
+    // If the sequence is safe, increment the safe counter and add a safe message to the tracker
     if($isSafe) {
         $safeSum++;
         $safeTracker[] = 'Safe';
     }
-
 }
 
-//print_r($safeTracker);
+// Output the number of safe sequences
 echo $safeSum."\n";
-exit;
-
-//The levels are either all increasing or all decreasing.
-//Any two adjacent levels differ by at least one and at most three.
--- Day2/part2.php
+++ Day2/part2.php
@@ -1,16 +1,25 @@
 <?php
 
+// Rules:
+// The levels mist be either all increasing or all decreasing.
+// Any two adjacent levels differ by at least one and at most three.
+// If successful with the removal of any one level in the set, it is still safe.
+
 $input_data = fopen("./input.txt", "r");
 
 $safeTracker = [];
 $safeSum = 0;
-
+// Read in each line one by one
 while (($line = fgets($input_data)) !== false) {        
+    // Get the levels from the line as an array of values
     $levels = explode(" ", $line);
+    // Assume unsafe to start with
     $isSafe = false;
+
     // Check the base level with no changes to remove the levels
     $isSafe = checkLevels($levels);
-
+    
+    // If it's not safe, check each set of levels where one is removed, looping to find a safe set    
     if(!$isSafe) {
         foreach($levels as $key => $level) {
             $levelclone = $levels;
@@ -22,6 +31,7 @@ while (($line = fgets($input_data)) !== false) {
         }
     }
 
+    // If safe (either with no changes, or with one level removed) increment the safe sum
     if($isSafe) {
         $safeSum++;
     }
@@ -29,35 +39,42 @@ while (($line = fgets($input_data)) !== false) {
 }
 
 
+// Function to check the levels are incremending/decrementing and within the correct range
 function checkLevels($levelsArray){
 
     echo 'Checking levels: '.implode(" ", $levelsArray)."\n";
-
+    // Assume the first level is the base level
     $track = $levelsArray[0];
+    // Assume we are increasing to start with
     $decreasing = false;
+    // assume safe to start with, we will check for unsafe conditions
     $isSafe = true;
 
+    // Check if we are decreasing or increasing to start with. This will determine the direction we are checking
     if($levelsArray[0] > $levelsArray[1]) {
         $decreasing = true;
     }
-    
+    // Loop through the levels to check they are increasing or decreasing and within the correct range
     foreach($levelsArray as $key => $level) {
-
+        // Skip the first level as we are using it as the base level
         if($key == 0) {
             continue;
         }
 
         // Check we are still decreasing if we started decreasing
         if($decreasing && $level > $track) {
+            // The levels are not decreasing but should be
             $isSafe = false;
             break;            
         }elseif(!$decreasing && $level < $track) {
+            // The levels are not increasing but should be
             $isSafe = false;
             break;
         }
 
         // Check the value between the numbers change is not greater than 3 and is more than 1
-        if(abs($track - $level) > 3 || abs($track - $level) < 1) {            
+        if(abs($track - $level) > 3 || abs($track - $level) < 1) {  
+            // The levels are not within the correct range          
             $isSafe = false;            
             break;
         }
@@ -66,12 +83,6 @@ function checkLevels($levelsArray){
     return $isSafe;
 }
 
+// Output the count of the safe levels
 echo $safeSum."\n";
 exit;
-
-//The levels are either all increasing or all decreasing.
-//Any two adjacent levels differ by at least one and at most three.
-
-// not 402 -- too low
-// not 409 -- too low
-// not 617 -- too high
