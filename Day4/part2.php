5b0eab4eb5c32018cab9ce813d6ae02c42fbd740
courtney-zinco <courtney.henderson@impactinternational.com>
2024-12-05 19:52:05 +0000

    Do part 2
    Do part 2 - find all A's as the middle item for a mas X - then check the surrounding characters and build a string from them going clockwise - Check if the pattern matches what we would expect for a mas in an X shape as defined in the rules

--- Day4/part2.php
+++ Day4/part2.php
@@ -1,46 +1,62 @@
 <?php
 
+// Open the input file in read mode
 $input_data = fopen("./input.txt", "r");
-$globalTotaller = [];
 
-while (($line = fgets($input_data)) !== false) {      
-    
+// Set error reporting to only show errors and parse errors
+error_reporting(E_ERROR | E_PARSE);
 
-    //Split the input by do() then string anything past don't() on each line
-    // This gives us everything past a do(), including the start of the input
-    // Then apply the same checkString function from part1 to each string and sum the results
-    $strings = explode('do()', $line);
-    foreach($strings as $string){
-        
-        $position = strpos($string, 'don\'t()');
-        if($position){
-            // Extract the part of the string before the comma
-            $result = substr($string, 0, $position);
-            $globalTotaller[] = checkString($result);
-        }else{
-            $globalTotaller[] = checkString($string);            
-        }
-    }
-    
-    echo array_sum($globalTotaller)."\n";
+// The following code is a brute force approach to find the MAS in an X format in the input data
+//  M.S
+//  .A.
+//  M.S
+// Or other permutations of the same
+
+// To do this, we find the A and check the surrounding 'corners' clockwise, if these strings match what we'd expect
+// for MAS in the X format, we increment the match counter
+
+// Initialize an empty matrix to store the input data
+$matrix = [];
+$matchCounter = 0;
+
+// Read the input file line by line
+while (($line = fgets($input_data)) !== false) {
+    // Split each line into characters and add to the matrix
+    $matrix[] = str_split(trim($line));
 }
 
-function checkString($string){
-    
-    $totaller = [];
-    if(preg_match_all('/mul\(\d+,\d+\)/', $string, $matches)) {
-        foreach($matches[0] as $match){
-            $numbers = explode(',', $match);
-            preg_match_all('/\d+/', $numbers[0], $num1Matches);
-            preg_match_all('/\d+/', $numbers[1], $num2Matches);
-
-            $num1 = $num1Matches[0][0];
-            $num2 = $num2Matches[0][0];
-                        
-            $totaller[] = $num1 * $num2;
-            
+// Iterate through each row and column of the matrix
+foreach($matrix as $row => $line) {
+    foreach($line as $column => $char) {
+        // If the character is 'A', start checking for the sequence
+        if($char == 'A') {
+            //echo "found an A at $row, $column\n";
+            // Check the surrounding characters and increment the match counter if the sequence is found
+            $matchCounter += checkSurrounding($matrix, $row, $column);
         }
     }
-    return array_sum($totaller);
 }
 
+// Output the total number of matches found
+echo $matchCounter."\n";
+
+
+function checkSurrounding($matrix, $row, $column) {
+  
+    //Check if the top left, then top right, then bottom right, then bottom left are either
+    // MSSM, SMMS MMSS, SSMM
+    // NOT SMSM MSMS -- Could probably make this better for checking the NOT rather than the IS
+
+    $topLeft   = $matrix[$row-1][$column-1];
+    $topRight  = $matrix[$row-1][$column+1];
+    $bottomRight = $matrix[$row+1][$column+1];
+    $bottomLeft = $matrix[$row+1][$column-1];
+
+    // Build the string of the surrounding characters
+    $string = $topLeft.$topRight.$bottomRight.$bottomLeft;    
+    // Check if a valid format is found
+    if($string == "MSSM" || $string == "SMMS" || $string == "MMSS" || $string == "SSMM") {
+        return 1;
+    }    
+    return 0;
+}
