14a0c4fb31a60a089c015795a564cedefdfebddf
courtney-zinco <courtney.henderson@impactinternational.com>
2024-12-05 09:12:28 +0000

    Add commentary to day 1
    Add commentary to day 1 part 1 and 2

--- Day1/part1.php
+++ Day1/part1.php
@@ -2,21 +2,32 @@
 
 $input_data = fopen("./input.txt", "r");
 
+// Define 2 empty lists to store the numbers
 $list1 = [];
 $list2 = [];
+
+// Define an empty list to store the distances
 $distances = [];
 
+// Read in each line one by one
 while (($line = fgets($input_data)) !== false) {        
+    // Split the line into an array of just the numbers
     $lineArray = explode(" ", $line);
+    // Add the first and last numbers to the lists
     $list1[] = $lineArray[0];
     $list2[] = $lineArray[3];
 }
 
+// Sort the two lists by value
 sort($list1);
 sort($list2);
 
+// Loop the lists and calculate the distance between the two values
+// Absolute value is used to ensure the distance is always positive
 foreach($list1 as $key => $value) {
+    // Store the value in the distances list
     $distances[] = abs($list1[$key] - $list2[$key]);
 }
 
+// Output the sum of the distances
 echo array_sum($distances);
--- Day1/part2.php
+++ Day1/part2.php
@@ -2,22 +2,33 @@
 
 $input_data = fopen("./input.txt", "r");
 
+// Define 2 empty lists to store the numbers
 $list1 = [];
 $list2 = [];
-$calcVals = [];
 
+// Define an empty list to store the distances
+$distances = [];
+
+// Read in each line one by one
 while (($line = fgets($input_data)) !== false) {        
+    // Split the line into an array of just the numbers
     $lineArray = explode(" ", $line);
+    // Add the first and last numbers to the lists
     $list1[] = $lineArray[0];
-    $list2[] = trim($lineArray[3]);
+    $list2[] = $lineArray[3];
 }
 
+// Count the values in list2 and store them in an array
 $countedValues = array_count_values($list2);
 
+// For each value in list1, multiply it by the count of that value in list2
 foreach($list1 as $key=>$list1Value) {
+    // If the value exists in the counted values array, multiply it by the count
     if(isset($countedValues[$list1Value])){
+        // Store the calculated value in an array
         $calcVals[] = $list1Value * $countedValues[$list1Value];
     }
 }
 
+// Output the sum of the calculated values
 echo array_sum($calcVals);
