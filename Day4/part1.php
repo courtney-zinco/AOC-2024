c10971ea6a0557d0606773f7a81966daa1054d6d
courtney-zinco <courtney.henderson@impactinternational.com>
2024-12-05 09:26:31 +0000

    Add commentary to day 4
    Add commentary to day 4, part 1 (part 2 is not yet complete)

--- Day4/part1.php
+++ Day4/part1.php
@@ -1,126 +1,148 @@
 <?php
 
+// Open the input file in read mode
 $input_data = fopen("./input.txt", "r");
 
+// Set error reporting to only show errors and parse errors
 error_reporting(E_ERROR | E_PARSE);
 
+// The following code is a brute force approach to find the word "XMAS" in the input data
+// The idea is to look for the letter 'X' and then check the surrounding characters for 'M'
+// If 'M' is found, check the surrounding characters for 'A'
+// If 'A' is found, check the surrounding characters for 'S'
+// If 'S' is found, log it as a successful match
+// Continue until all characters in the input file are checked
 
-// brute force - could go every character for an X (first letter of xmas) 
-// then check every character surrounding it for an M
-// if found, check every character surrounding the M for an A
-// if found, check every character surrounding the A for an S
-// If we have an S log it as a successful match 
-// Continue until we have checked all characters in the input file
-
-// First build a matrix of the input data
+// Initialize an empty matrix to store the input data
 $matrix = [];
 $matchCounter = 0;
-while (($line = fgets($input_data)) !== false) {      
-    $matrix[] = str_split(trim($line));        
+
+// Read the input file line by line
+while (($line = fgets($input_data)) !== false) {
+    // Split each line into characters and add to the matrix
+    $matrix[] = str_split(trim($line));
 }
 
-
-// This is a decent start, but it doesn't account for the fact that the matches must be linear!
-foreach($matrix as $row=>$line){
-    foreach($line as $column=>$char){        
-        if($char == 'X'){            
-            echo "found an X at $row, $column\n";            
-            $matchCounter+= checkSurrounding($matrix, $row, $column, 'M');                             
+// Iterate through each row and column of the matrix
+foreach($matrix as $row => $line) {
+    foreach($line as $column => $char) {
+        // If the character is 'X', start checking for the sequence
+        if($char == 'X') {
+            echo "found an X at $row, $column\n";
+            // Check the surrounding characters for 'M' and continue the sequence check
+            $matchCounter += checkSurrounding($matrix, $row, $column, 'M');
         }
     }
 }
+
+// Output the total number of matches found
 echo "\n".$matchCounter."\n";
-function checkSurrounding($matrix, $row, $column, $char){
+
+// Function to check the surrounding characters for the given character
+function checkSurrounding($matrix, $row, $column, $char) {
     $matches = 0;
-    if($matrix[$row-1][$column-1] == $char){      
-        echo "found a $char in the top left\n";          
-        // check the line going in the direction of the M for a A
-        if($matrix[$row-2][$column-2] == 'A'){     
-            echo "found an A in the top left\n";       
-            // check the line going in the direction of the A for a S
-            if($matrix[$row-3][$column-3] == 'S'){               
+
+    // Check top-left direction
+    if($matrix[$row-1][$column-1] == $char) {
+        echo "found a $char in the top left\n";
+        // Check for 'A' in the same direction
+        if($matrix[$row-2][$column-2] == 'A') {
+            echo "found an A in the top left\n";
+            // Check for 'S' in the same direction
+            if($matrix[$row-3][$column-3] == 'S') {
                 echo "found an S in the top left\n";
-                echo 'SUCCESSFUL MATCH FOUND';                 
+                echo 'SUCCESSFUL MATCH FOUND';
                 $matches++;
             }
         }
-        
     }
-    if($matrix[$row-1][$column] == $char){
+
+    // Check top direction
+    if($matrix[$row-1][$column] == $char) {
         echo "found a $char in the top\n";
-        if($matrix[$row-2][$column] == 'A'){            
+        if($matrix[$row-2][$column] == 'A') {
             echo "found an A in the top\n";
-            if($matrix[$row-3][$column] == 'S'){           
+            if($matrix[$row-3][$column] == 'S') {
                 echo "found an S in the top\n";
-                echo 'SUCCESSFUL MATCH FOUND';                     
+                echo 'SUCCESSFUL MATCH FOUND';
                 $matches++;
             }
         }
-    } 
-    if($matrix[$row-1][$column+1] == $char){
+    }
+
+    // Check top-right direction
+    if($matrix[$row-1][$column+1] == $char) {
         echo "found a $char in the top right\n";
-        if($matrix[$row-2][$column+2] == 'A'){  
-            echo "found an A in the top right\n";          
-            if($matrix[$row-3][$column+3] == 'S'){                                
+        if($matrix[$row-2][$column+2] == 'A') {
+            echo "found an A in the top right\n";
+            if($matrix[$row-3][$column+3] == 'S') {
                 echo "found an S in the top right\n";
                 echo 'SUCCESSFUL MATCH FOUND';
                 $matches++;
             }
         }
     }
-    if($matrix[$row][$column-1] == $char){
-        echo "found a $char in the left\n";
-        if($matrix[$row][$column-2] == 'A'){   
-            echo "found an A in the left\n";         
-            if($matrix[$row][$column-3] == 'S'){     
-                echo "found an S in the left\n";                           
-                $matches++;
-            }
-        }
-    }
-    if($matrix[$row][$column+1] == $char){
-        echo "found a $char in the right\n";
-        if($matrix[$row][$column+2] == 'A'){    
-            echo "found an A in the right\n";        
-            if($matrix[$row][$column+3] == 'S'){     
-                echo "found an S in the right\n";                           
-                $matches++;
-            }
-        }
-    }
-    if($matrix[$row+1][$column-1] == $char){
-        echo "found a $char in the bottom left\n";
-        if($matrix[$row+2][$column-2] == 'A'){        
-            echo "found an A in the bottom left\n";    
-            if($matrix[$row+3][$column-3] == 'S'){     
-                echo "found an S in the bottom left\n";                           
-                $matches++;
-            }
-        }
-    }
-    if($matrix[$row+1][$column] == $char){
-        echo "found a $char in the bottom\n";
-        if($matrix[$row+2][$column] == 'A'){        
-            echo "found an A in the bottom\n";    
-            if($matrix[$row+3][$column] == 'S'){   
-                echo "found an S in the bottom\n";                             
-                $matches++;
-            }
-        }
-    }
-    if($matrix[$row+1][$column+1] == $char){
-        echo "found a $char in the bottom right\n";
-        if($matrix[$row+2][$column+2] == 'A'){      
-            echo "found an A in the bottom right\n";      
-            if($matrix[$row+3][$column+3] == 'S'){  
-                echo "found an S in the bottom right\n";                              
-                $matches++;
-            }
-        }
-    }
-    return $matches;    
-}
 
-// 2312 is too low
-// 2427 is Correct
-// 5824 is too high
+    // Check left direction
+    if($matrix[$row][$column-1] == $char) {
+        echo "found a $char in the left\n";
+        if($matrix[$row][$column-2] == 'A') {
+            echo "found an A in the left\n";
+            if($matrix[$row][$column-3] == 'S') {
+                echo "found an S in the left\n";
+                $matches++;
+            }
+        }
+    }
+
+    // Check right direction
+    if($matrix[$row][$column+1] == $char) {
+        echo "found a $char in the right\n";
+        if($matrix[$row][$column+2] == 'A') {
+            echo "found an A in the right\n";
+            if($matrix[$row][$column+3] == 'S') {
+                echo "found an S in the right\n";
+                $matches++;
+            }
+        }
+    }
+
+    // Check bottom-left direction
+    if($matrix[$row+1][$column-1] == $char) {
+        echo "found a $char in the bottom left\n";
+        if($matrix[$row+2][$column-2] == 'A') {
+            echo "found an A in the bottom left\n";
+            if($matrix[$row+3][$column-3] == 'S') {
+                echo "found an S in the bottom left\n";
+                $matches++;
+            }
+        }
+    }
+
+    // Check bottom direction
+    if($matrix[$row+1][$column] == $char) {
+        echo "found a $char in the bottom\n";
+        if($matrix[$row+2][$column] == 'A') {
+            echo "found an A in the bottom\n";
+            if($matrix[$row+3][$column] == 'S') {
+                echo "found an S in the bottom\n";
+                $matches++;
+            }
+        }
+    }
+
+    // Check bottom-right direction
+    if($matrix[$row+1][$column+1] == $char) {
+        echo "found a $char in the bottom right\n";
+        if($matrix[$row+2][$column+2] == 'A') {
+            echo "found an A in the bottom right\n";
+            if($matrix[$row+3][$column+3] == 'S') {
+                echo "found an S in the bottom right\n";
+                $matches++;
+            }
+        }
+    }
+
+    // Return the number of matches found
+    return $matches;
+}
